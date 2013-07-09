886be700b58b746c808198d68b53d9843c0706d5
kashi.zeeshan <kashi.zeeshan@gmail.com>
2013-07-10 00:27:29 +0500

    Updated web.js file

--- web.js
+++ web.js
@@ -3,14 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 
-var fs = require('fs');
-var data = fs.readFileSync("index.html", "utf8");
-console.log(data);
-
 app.get('/', function(request, response) {
-  response.send("");
-});
+    var fs = require('fs');
 
+    var buf = new Buffer (fs.readFileSync("index.html", "utf-8"));
+
+    response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
