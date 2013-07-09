d7b166d8bc2a8b670842b439b944f6d8fe4f01b8
kashi.zeeshan <kashi.zeeshan@gmail.com>
2013-07-10 00:23:30 +0500

    Updated web.js file

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf8");
-
+console.log(data);
 
 app.get('/', function(request, response) {
   response.send("");
