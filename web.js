d1ff8deb45c1aab83a438d7757acc32714cd2312
kashi.zeeshan <kashi.zeeshan@gmail.com>
2013-07-10 00:08:32 +0500

    Updated web.js file

--- web.js
+++ web.js
@@ -1,19 +1,7 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-
-
 
 var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf8");
+response.send(data);
 
-app.get('/', function(request, response) {
-  response.send(data);
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
 
