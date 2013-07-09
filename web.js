402e90a1182ba423eca5114d7396c42e9d2985a7
kashi.zeeshan <kashi.zeeshan@gmail.com>
2013-07-10 00:15:45 +0500

    Updated web.js file

--- web.js
+++ web.js
@@ -7,6 +7,9 @@ var fs = require('fs');
 var data = fs.readFileSync("index.html", "utf8");
 response.send(data);
 
+app.get('/', function(request, response) {
+  response.send(data);
+});
 
 
 var port = process.env.PORT || 5000;
